1952f22d2590f6dd21013a83417adcda082a7722
glebbogachev00 <glebbogachev00@gmail.com>
2025-09-04 14:01:59 +0700

    feat: Update quotes to specific entries
    - Modified src/data/quotes.js to include only two specific quotes as requested.

--- src/data/quotes.js
+++ src/data/quotes.js
@@ -1,42 +1,10 @@
 export const CREATIVE_QUOTES = [
   {
-    text: "Productivity gurus hate this one trick: actually finishing thoughts instead of organizing them to death",
-    author: "stream"
+    text: "If you treat things with care, in time, the soul will inhibit them",
+    author: "regto"
   },
   {
-    text: "While everyone else is color-coding folders, you're creating",
-    author: "stream"
-  },
-  {
-    text: "Your brain moves too fast for their systems. Good",
-    author: "stream"
-  },
-  {
-    text: "Scattered minds create the most interesting connections",
-    author: "stream"
-  },
-  {
-    text: "Chaos isn't a bug, it's a feature",
-    author: "stream"
-  },
-  {
-    text: "Normal people use folders. Creative people use streams",
-    author: "stream"
-  },
-  {
-    text: "Traditional organization is for traditional minds",
-    author: "stream"
-  },
-  {
-    text: "The real clutter isn't your notes - it's everyone else's productivity advice",
-    author: "stream"
-  },
-  {
-    text: "stream rewards the curious",
-    author: "stream"
-  },
-  {
-    text: "Built by a scattered mind, for scattered minds",
-    author: "stream"
+    text: "Enjoy the little detours to the fullest. That is where you'll find the things more important than what you want",
+    author: "ging freecss"
   }
 ];
